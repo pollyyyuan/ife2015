1f104cab9542a2bb5f84e42a91bf69356d809255
polly <polly_yyuan@163.com>
2017-02-14 19:32:12 +0800

    update

--- task3/js/init.js
+++ task3/js/init.js
@@ -1,15 +1,15 @@
 ;(function(){
 	// 模拟数据
-	var data=[{
+	var dataa=[{
 		folderName:'默认列表',
 		tasks:[{
 			tasksName:'task1',
 			task:[]
 		}]
-	}]
+	}];
 	//初始化
 	//localStorage.removeItem('mytask');
-	// console.log(myStorage);
+	 console.log(localStorage.mytask);
 	var Main=(function(){
 		function Main(dom1,dom2,dom3){
 			this.defaults=[
@@ -31,7 +31,6 @@
 				}
 				else{
 					this.myData=Data.getData();
-					console.log(localStorage.mytask);
 				}
 				//绑定dom
 				this.bindDoms();
@@ -54,9 +53,8 @@
 		}
 	};
 	// 对话框模块
-	var Dialog=function(me,addBtn,dialog,okFn){
-		var input=dialog.parentNode.querySelector('.add-input'),
-			message=dialog.querySelector('message');
+	var Dialog=function(addBtn,dialog){
+		var input=dialog.parentNode.querySelector('.add-input');
 		addBtn.addEventListener('click',function(){
 			dialog.style.display='block';
 			input.focus();
@@ -69,85 +67,114 @@
 			dialog.style.display='none';
 			input.value='';
 		});
-		dialog.querySelector('.ok-btn').addEventListener('click',function(){
-			if(input.value){
-				okFn(me,input.value,message);
-				dialog.style.display='none';
-				input.value='';
-			}
-		});
+
 	};
 	// 分类模块
 	var Classic=(function(){
-		function Classic(data){
+		function Classic(data){//Datas[]
 			this.myData=data;
 			this.numAllTask=0;
-			this.domStr='';
 		}
 		Classic.prototype={
 			init:function(classicDom,taskDom){
 				// 获取classicDom
 				this.classicDom=classicDom;
 				//绑定个数
-				this.bindNumAllTask();
-				//绑定列表
-				for(var folder in this.myData)
-				{	
-					this.bindDom(this.myData[folder]);
-				}
-				this.classicDom.innerHTML=this.domStr;
+				// this.bindNumAllTask();
+				//绑定列表s
+				this.bindDom();
+		
 				// 绑定事件
-				this.bindEvent();
-				//绑定任务列表
-				this.bindTask(taskDom);
+				 this.bindEvent();
+				//绑定Taskss
+				this.tasks=new Tasks();
+			},
+			//绑定单个列表
+			bindDom:function(){
+				var str='';
+				for(var i=0;i<this.myData.length;i++)
+				{	
+					var folderData=this.myData[i];
+					str+='<li>';
+					if(folderData.folderName){				
+						str+='<h3 class="h-classic">';
+						str+='<a href="#"><i class="icon-folder"></i><span>'+folderData.folderName+'</span>(<i class="id-list">0</i>)';
+						str+='<button class="addTask-btn"><i class="icon-add"></i></button>';		
+						str+='<button class="del-btn"><i class="icon-del"></i></button>';		
+						str+='</a></h3>';
+					}
+					if(folderData.tasks){	
+						str+='<ul class="list">';
+						for(var j=0;j<folderData.tasks.length;j++)
+						{	
+							var tasksData=folderData.tasks[j];
+							if(tasksData.tasksName){
+								str+='<li>';
+								str+='<a href="#"><i class="icon-file"></i><span>'+tasksData.tasksName+'</span>(<i>0</i>)';
+								str+='<button class="del-btn"><i class="icon-del"></i></button>';
+								str+='</a></li>';
+							}
+						}
+						str+='</ul>';	
+					}
+					str+='</li>';
+				}
+				this.classicDom.innerHTML=str;		
 			},
 			bindEvent:function(){
 				var me=this;
 				//folder
 				var lis=me.classicDom.querySelectorAll('li');
 				for(var li=0;li<lis.length;li++){
-					var a=lis[li].querySelector('.h-classic a');
+					var a=lis[li].querySelector('.h-classic a'),//获得标题
+						list=lis[li].querySelector('.list'),
+						tasksDom=lis[li].querySelectorAll('.list li');//获得task
+						console.log(a);
 					a.addEventListener('mouseover',function(){
+						var two=this;
 						var addTasksBtn=this.querySelector('.addTask-btn'),
 							delBtn=this.querySelector('.del-btn');
 						delBtn.style.display='block',
 						addTasksBtn.style.display='block';
-						addTasksBtn.addEventListener('click',function(){
-							var TasksDialog=document.getElementById('addTasksBox');
-							function tasksOkFn(me,input,message){
-								for(var tasks in{	
-									if(tasks==input)
+						//dialog
+						var TasksDialog=document.getElementById('addTasksBox');
+						Dialog(addTasksBtn,TasksDialog);
+						TasksDialog.querySelector('.ok-btn').addEventListener('click',function(){
+							var input=TasksDialog.querySelector('input'),
+								message=TasksDialog.querySelector('message');
+							if(input.value){
+								for(var t=0;t<tasksDom.length;t++){	
+									if(tasksDom[t].value==input.value)
 									{
 										message.value='该任务集已存在！';
 										return;
 									}
 								}
-								me.addTasks(input);
+								me.tasks.addDom(input.value,list,two.querySelector('span').innerHTML);
+								TasksDialog.style.display='none';
+								input.value='';
 							}
-							Dialog(a,this,TasksDialog,tasksOkFn);
-						});
+						});			
 					});
 					a.addEventListener('mouseout',function(){
 						this.querySelector('.del-btn').style.display='none';
 						this.querySelector('.addTask-btn').style.display='none';
 					});
-					
 				}
-				//绑定add按钮
-				var ClassicDialog=document.getElementById('addClassicBox'),
-					addBtn=document.getElementById('addClassicBtn');
-				function classicOkFn(me,input,message){
-					for(var folder in me.myData){	
-						if(folder==input)
-						{
-							message.value='该分类名已存在！';
+				//addDialog
+				var addDialog=document.getElementById('addClassicBox'),
+					addClassicBtn=document.getElementById('addClassicBtn'),
+					addinput=addDialog.querySelector('.input');
+				Dialog(addClassicBtn,addDialog);
+				addDialog.querySelector('.ok-btn').addEventListener('click',function(){
+					for(var dt=0;dt<me.myData.length;dt++){
+						if(my.myData[dt].folderName==input.value){
+							input.value='';
 							return;
 						}
 					}
-					me.addFolder(input);
-				}
-				Dialog(me,addBtn,ClassicDialog,classicOkFn);
-
+					this.addFolder(input.value);
+				});
 			},
 			addFolder:function(name){
 				var li=document.createElement('li');
@@ -169,26 +196,6 @@
 				var dom=document.getElementById('num-allTask');
 				dom.innerHTML=this.numAllTask;
 			},
-			bindDom:function(data){
-				this.domStr+='<li>';
-				if(data.folderName){				
-					this.domStr+='<h3 class="h-classic">';
-					this.domStr+='<a href="#"><i class="icon-folder"></i>'+data.folderName+'(<i class="id-list">0</i>)';
-					this.domStr+='<button class="addTask-btn"><i class="icon-add"></i></button>';		
-					this.domStr+='<button class="del-btn"><i class="icon-del"></i></button>';		
-					this.domStr+='</a></h3>';
-				}
-				if(data.tasks){
-					this.domStr+='<ul class="list">';
-					for(var tasksName in data.tasks)
-					{	
-						var tasks=new Tasks();
-						this.domStr+=tasks.bindDom(data.tasks[tasksName]);
-					}
-					this.domStr+='</ul>';
-					this.domStr+='</li>';		
-				}
-			},
 			bindTask:function(taskDom){
 				if(this.myData[0].tasks){
 					this.task=new Task(this.myData[0].tasks[0]);
@@ -200,120 +207,97 @@
 	})();
 	//任务集模块
 	var Tasks=(function(){
-		function Tasks(data){
-			this.myData=data;
+		function Tasks(){//tasks[]
+			//this.myData=data;
 			// this.numAllTask=0;
-			this.domStr='';
 		}	
 		Tasks.prototype={
 			init:function(tasksDom){
-				// 获取classicDom
 				this.tasksDom=tasksDom;
-				//绑定列表
-				for(var tasksin this.myData)
-				{	
-					this.bindDom(this.myData[tasks]);
-				}
-				this.tasksDom.innerHTML=this.domStr;
 				// 绑定事件
 				this.bindEvent();
 				// 绑定任务列表
 				// this.bindTask(taskDom);
 			},
+			addDom:function(data,listDom,spanStr){
+				var li=document.createElement('li');
+				var str='<a href="#"><i class="icon-file"></i>'+data+'(<i>0</i>)';
+				str+='<button class="del-btn"><i class="icon-del"></i></button>';
+				str+='</a></li>';
+				li.innerHTML=str;
+				listDom.appendChild(li);
+				var tasks={
+					tasksName:data,
+					task:[]
+				},
+				myData=Data.getData();
+				console.log(myData.length);
+				for(var i=0;i<myData.length;i++){
+					console.log('spanstr'+spanStr);
+					console.log('folderName'+myData[i].folderName);
+					if(myData[i].folderName==spanStr)
+					{
+						myData[i].tasks.push(tasks);
+						console.log(myData[i].tasks);
+						Data.updateStorage(myData);
+					}
+				}
+				this.init(listDom);
+			},
 			bindEvent:function(){
 				var me=this;
 				//tasks
-				var lis=me.classicDom.querySelectorAll('li');
+				var lis=me.tasksDom.querySelectorAll('li');
+				console.log(lis);
 				for(var li=0;li<lis.length;li++){
-					var a=lis[li].querySelector('.h-classic a');
-					a.addEventListener('mouseover',function(){
-						var addTasksBtn=this.querySelector('.addTask-btn'),
-							delBtn=this.querySelector('.del-btn');
-						delBtn.style.display='block',
-						addTasksBtn.style.display='block';
-						addTasksBtn.addEventListener('click',function(){
-							var TasksDialog=document.getElementById('addTasksBox');
-							function tasksOkFn(me,input,message){
-								for(var tasks in{	
-									if(tasks==input)
-									{
-										message.value='该任务集已存在！';
-										return;
-									}
-								}
-								me.addTasks(input);
-							}
-							Dialog(a,this,TasksDialog,tasksOkFn);
-						});
+					lis[li].addEventListener('mouseover',function(){
+						var delBtn=this.querySelector('.del-btn');
+						delBtn.style.display='block';
 					});
-					a.addEventListener('mouseout',function(){
+					lis[li].addEventListener('mouseout',function(){
 						this.querySelector('.del-btn').style.display='none';
-						this.querySelector('.addTask-btn').style.display='none';
-					});
-					
-				}
-				//绑定add按钮
-				var ClassicDialog=document.getElementById('addClassicBox'),
-					addBtn=document.getElementById('addClassicBtn');
-				function classicOkFn(me,input,message){
-					for(var folder in me.myData){	
-						if(folder==input)
-						{
-							message.value='该分类名已存在！';
-							return;
-						}
-					}
-					me.addFolder(input);
-				}
-				Dialog(me,addBtn,ClassicDialog,classicOkFn);
-
-			},
-			addTasks:function(name){
-				var li=document.createElement('li');
-				li.setAttribute('class','active');
-				var str='<a href="#"><i class="icon-file"></i>'+name+'(<i>0</i>)';
-				str+='<button class="del-btn"><i class="icon-del"</i></button>';
-				str+='</a></li>';
-				li.innerHTML=str;
-				this.classicDom.querySelector('.list').appendChild(li);
-				var data={
-					tasksName:name,
-					task:[]
-				};
-				this.myData.push(data);
-				Data.updateStorage(this.myData);
-			},
-			addTasks:function(name){
-				var li=document.createElement('li');
-				li.setAttribute('class','active');
-				var str='<a href="#"><i class="icon-file"></i>'+name+'(<i>0</i>)';
-				str+='<button class="del-btn"><i class="icon-del"</i></button>';
-				str+='</a></li>';
-				li.innerHTML=str;
-				this.classicDom.querySelector('.list').appendChild(li);
-				var data={
-					tasksName:name,
-					task:[]
-				};
-				this.myData.push(data);
-				Data.updateStorage(this.myData);
-			},
-			bindDom:function(data){
-				var domStr='';
-				domStr+='<li>';
-				domStr+='<a href="#"><i class="icon-file"></i>'+task+'(<i>0</i>)';
-				domStr+='<button class="del-btn"><i class="icon-del"></i></button>';
-				domStr+='</a></li>';
-				return domStr;
-			},
-			bindTask:function(taskDom){
-				if(this.myData[0].tasks){
-					this.task=new Task(this.myData[0].tasks[0]);
-					this.task.init(taskDom);
+					});	
 				}
 			}
+			// addTasks:function(name){
+			// 	var li=document.createElement('li');
+			// 	li.setAttribute('class','active');
+			// 	var str='<a href="#"><i class="icon-file"></i>'+name+'(<i>0</i>)';
+			// 	str+='<button class="del-btn"><i class="icon-del"</i></button>';
+			// 	str+='</a></li>';
+			// 	li.innerHTML=str;
+			// 	this.classicDom.querySelector('.list').appendChild(li);
+			// 	var data={
+			// 		tasksName:name,
+			// 		task:[]
+			// 	};
+			// 	this.myData.push(data);
+			// 	Data.updateStorage(this.myData);
+			// },
+			// addTasks:function(name){
+			// 	var li=document.createElement('li');
+			// 	li.setAttribute('class','active');
+			// 	var str='<a href="#"><i class="icon-file"></i>'+name+'(<i>0</i>)';
+			// 	str+='<button class="del-btn"><i class="icon-del"</i></button>';
+			// 	str+='</a></li>';
+			// 	li.innerHTML=str;
+			// 	this.classicDom.querySelector('.list').appendChild(li);
+			// 	var data={
+			// 		tasksName:name,
+			// 		task:[]
+			// 	};
+			// 	this.myData.push(data);
+			// 	Data.updateStorage(this.myData);
+			// },
+			
+			//,bindTask:function(taskDom){
+			// 	if(this.myData[0].tasks){
+			// 		this.task=new Task(this.myData[0].tasks[0]);
+			// 		this.task.init(taskDom);
+			// 	}
+			// }
 		}
-		return Classic;
+		return Tasks;
 	})();
 	//任务模块
 	var Task=(function(){
